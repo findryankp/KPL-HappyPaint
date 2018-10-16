76e9379cc41b230f4ade7aa59cf8a5e9d6af99dc
Unknown <findryankurnia@gmail.com>
2018-10-16 19:49:18 +0700

    File File yang diperlukan

--- HappyPaint/HappyPaint/MainWindow.cs
+++ HappyPaint/HappyPaint/MainWindow.cs
@@ -7,14 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace HappyPaint
 {
-    public partial class Form1 : Form
+    public partial class MainWindow : Form
     {
-        public Form1()
+        public MainWindow()
         {
             InitializeComponent();
+            InitUI();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitUI()
+        {
         }
     }
 }
